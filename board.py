48ddba25ff10fa971682d69299403fc4a0f87e43
Dan Murphy <dan.murphy250317@gmail.com>
2024-02-20 11:00:09 +0000

    20th feb

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 from CONST import *
 from square import Square
+from piece import *
 
 class board:
 
@@ -14,7 +15,18 @@ class board:
                 self.squares [row] [col] = Square(row, col)
 
     def _add_pieces(self, color):
-        pass
+        if color == 'White':
+            row_pawn, row_other = (6, 7)
+        else:
+            row_pawn, row_other = (1, 0)
+
+        for col in range(COLS):
+            self.squares[row_pawn][col] = Square(row_pawn, col, Pawn(color))
+
+        self.squares[row_other][1] = Square(row_other, 1, Knight(color))
+        self.squares[row_other][6] = Square(row_other, 6, Knight(color))
+
+
 
 b = board()
 b._create()
--- piece.py
+++ piece.py
@@ -1,3 +1,4 @@
+import os
 
 class Piece:
     def __init__(self, name, color, value, texture=None, texture_rect=None):
@@ -10,9 +11,19 @@ class Piece:
             value_sign = -1
 
         self.value = value * value_sign
+        self.moves = []
+        self.moved = False
         self.texture = texture
         self.set_texture()
         self.texture_rect = texture_rect
+
+    def set_texture(self, size=80):
+        self.texture = os.path.join(
+            f'assets/images/imgs-{size}px/{self.color}_{self.name}.png'
+        )
+
+    def add_moves(self, move):
+        self.moves.append(move)
 
 class Pawn(Piece):
     def __init__(self, color):
